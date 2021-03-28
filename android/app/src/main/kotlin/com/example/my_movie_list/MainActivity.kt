9b45218d877d1ded4abdb52cc1e9ae787b61fdeb
sr-Te <jorge.eff.96@gmail.com>
2021-03-28 19:00:59 -0300

    folder organization n pubs

---
+++ android/app/src/main/kotlin/com/example/my_movie_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_movie_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/features/movies/presentation/views/movies_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MoviesView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Películas'),
+      ),
+    );
+  }
+}
